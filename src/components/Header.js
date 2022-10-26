b8d056625590659211060ab5581ff145e00f5597
shehbaz khanani <cuteboyusman49@gmail.com>
2022-10-26 15:10:02 +0500

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
-import { AppBar, Typography } from "@mui/material";
+import { AppBar, Box} from "@mui/material";
+import todoImg from '../images/todo.jpg'
+import LogoutIcon from '@mui/icons-material/Logout';
+import { Link } from "react-router-dom";
 
 function Header (){
    
    return (
    <>
    <AppBar sx={{textAlign: "center", backgroundColor : "black"}}>
+<Box sx={{display : "flex", justifyContent : "space-between"}}> 
 
-<Typography variant="h4"> Todo </Typography>
+<img src={todoImg} width="120wh" height="80vh" />
 
+<Link to="/" style={{color : "white", textDecoration : "none"}}> <LogoutIcon sx={{fontSize : "2.5rem", marginTop : 2}}/> </Link>
+
+</Box>
    </AppBar>
     
     </>
--- src/screen/Todo.js
+++ src/screen/Todo.js
@@ -1,4 +1,3 @@
-import { isValidDateValue } from '@testing-library/user-event/dist/utils';
 import { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
@@ -6,35 +5,44 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SaveAltIcon from '@mui/icons-material/SaveAlt';
 import { Box, TextField, Button } from '@mui/material';
-import { getDatabase, onValue, push, ref, update,} from "firebase/database";
+import { getDatabase, onValue, push, ref, remove, update, } from "firebase/database";
 import app from '../config/Firebaseconfig';
-import { useLocation } from 'react-router-dom';
+import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 
 
 function todu() {
+    const Auth = getAuth(app);
     let [txt, setTxt] = useState("")
     let [list, setList] = useState([]);
-    
-    const todoData = (key, value) => {
-    const editRef = ref(database, `todos/${user.uid}/${key}`)  
-    update(editRef, { value } ).then((succ) => {
-  console.log(succ);
-    }).catch((err)=> {
-        console.log(err);
-    })
-}
-
     const location = useLocation();
-    console.log("LOCATION", location)
-    const user = location.state.success;
+    // console.log("LOCATION", location)
+    const user = location?.state?.success;
+    const navigate = useNavigate()
+    const date = new Date();
+    const hour = date.getHours();
+    const second = date.getSeconds();
+    const minutes = date.getMinutes();
+
+    const time = `${hour} : ${minutes} : ${second}`;
+
+    const todoData = (key, value) => {
+        const editRef = ref(database, `todos/${user.uid}/${key}`)
+        update(editRef, { value, time }).then((succ) => {
+            console.log(succ);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
     const editButton = (index) => {
         setList(list.map((values, i) => {
-            console.log(values, "updatedValue");
             if (index === i) {
-               if (values.edit) {
-                todoData(values.id, values.value) 
-               }
+                if (values.edit) {
+                    todoData(values.id, values.value)
+                }
                 return {
                     ...values,
                     edit: values.edit ? false : true,
@@ -54,53 +62,92 @@ function todu() {
     const database = getDatabase(app);
 
     const add = () => {
-        // setList([...list, { value: txt }])
         setTxt("");
         const todoRef = ref(database, `todos/${user.uid}`);
         const todo = {
             value: txt,
-            // uid: user.uid
+            time: time
         }
         push(todoRef, todo)
     }
 
-    useEffect(()=>{
-        const reference = ref(database, `todos/${user.uid}`);
+    useEffect(() => {
+        onAuthStateChanged(Auth, (user) => {
+        if(user) {
+         const reference = ref(database, `todos/${user.uid}`);
         onValue(reference, (event) => {
-          const todos = event.val();
-          if (todos) {
-            let editValue = Object.entries(todos).map(([key, value])=> {
-           return {...value, id : key}
-            })
-            setList(editValue)
-          
-        }})
-    },[])
-
-
-    let deleteValue;
-    const deleteButton = (index) => {
-        const deletelist = [...list]
-        deleteValue = deletelist.filter((todoValue, indexValue) => {
-            if (index !== indexValue) {
-                return todoValue;
+            const todos = event.val();
+            const status = event.exists();
+            if (status) {
+                let editValue = Object.entries(todos).map(([key, value]) => {
+                    return { ...value, id: key }
+                })
+                setList(editValue)
             }
+        }) 
+        }else {
+                navigate('/')
+            }
+       
+    })
+    }, [])
+
+    const deleteall = () => {
+        const reference = ref(database, `todos/${user.uid}`);
+        remove(reference).then((succ) => {
+            let allDeleteValue = list.filter((todoValue,) => {
+                if (!todoValue.id) {
+                    return { ...todoValue };
+                }
+            })
+            setList(allDeleteValue)
+        }).catch((err) => {
+            console.log(err, "masla he");
         })
-        setList(deleteValue)
+
     }
+
+    const deleteButton = (event) => {
+        //    console.log(event, index, "event");
+        const reference = ref(database, `todos/${user.uid}/${event.id}`);
+        remove(reference).then((succ) => {
+            let deleteValue = list.filter((todoValue,) => {
+                if (event.id !== todoValue.id) {
+                    return { ...todoValue };
+                }
+            })
+            setList(deleteValue)
+        }).catch((err) => {
+            console.log(err, "masla he");
+        })
+
+    }
+
+  const EditValue = (e) => {
+    if(e.key == 'Enter') {
+        editButton(e.target.value)
+    }
+  }
+
+   const EnterValue = (e) => {
+    if(e.key == 'Enter') {
+        add()
+    }
+   }
 
     const todolist = list.map((todo, index) => {
         return (
             <Box key={index} sx={{ textAlign: "center" }}>
-                <Box>
+                <Box className='d-flex justify-content-center my-5'>
 
-                    <li className='list2 fs-2 d-flex' key={index}> {todo.edit ? <TextField type="text" value={todo.val}
+                    <li className='list2 fs-2 d-flex border' style={{ width: "75%", justifyContent: "space-between" }} key={index}> {todo.edit ? <TextField width="100%" type="text" value={todo.value}
+                        onKeyDown={EditValue}
                         onChange={(inpVal) => {
                             setList(list.map((e, i) => {
                                 if (index == i) {
                                     return {
                                         ...e,
-                                        value : inpVal.target.value,
+                                        value: inpVal.target.value,
                                     }
                                 }
                                 else {
@@ -109,9 +156,10 @@ function todu() {
                             }))
                         }} /> : todo.value
                     }
+                    {todo.time}
                         <Box sx={{ textAlign: "center" }}>
-                            <Button onClick={() => editButton(index)} > {todo.edit ? <SaveAltIcon /> : <EditIcon />}   </Button>
-                            {!todo.edit && <Button onClick={() => deleteButton(index)}> <DeleteIcon /> </Button>}
+                            <Button onClick={() => editButton(index)} > {todo.edit ? <SaveAltIcon style={{ fontSize: "2rem", color: "black" }} /> : <EditIcon style={{ fontSize: "2rem", color: "black" }} />}   </Button>
+                            {!todo.edit && <Button onClick={() => deleteButton(todo, index)}> <DeleteIcon style={{ fontSize: "2rem", color: "black" }} /> </Button>}
                         </Box>
 
 
@@ -124,12 +172,13 @@ function todu() {
     return (
         <>
             <Header />
-            <Box sx={{ marginTop: 10, textAlign: "center" }}>
+            <Box sx={{ marginTop: 15, textAlign: "center" }}>
                 <TextField value={txt} onChange={(e) => {
                     setTxt(e.target.value)
-                }} sx={{ width: "70%" }} />
+                }} sx={{ width: "70%" }} onKeyDown={EnterValue} />
 
-                <Button onClick={add}>  <AddCircleOutlineIcon />  </Button>
+                <Button onClick={add}>  <AddCircleOutlineIcon style={{ fontSize: "3rem", color: "black" }} />  </Button>
+                <Button sx={{display : {xs : 'none', md : 'inline-block'}}} onClick={deleteall}>  <DeleteForeverIcon style={{ fontSize: "3rem", color: "black" }} /> </Button>
             </Box>
             {todolist}
         </>
